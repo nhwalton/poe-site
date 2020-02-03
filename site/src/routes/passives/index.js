7fb3338803ed1a76f088767d7483fd98d328317c
Nick Walton <nicholas.h.walton@gmail.com>
2020-02-03 00:31:07 -0600

    added gem icons and material inputs for build selection

--- site/src/routes/passives/index.js
+++ site/src/routes/passives/index.js
@@ -1,28 +1,71 @@
 import Preact, { h, Component, useState, useEffect } from 'preact';
+
+import Button from 'preact-material-components/Button';
 import Card from 'preact-material-components/Card';
+import TextField from 'preact-material-components/TextField';
+import 'preact-material-components/TextField/style.css';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
 import style from './style';
 
+const Quests = ({ quest }) => {
+    return (
+        <li style="list-style-type:none;">
+        {quest.name}
+        {quest.skillPoints}
+        </li>
+    );
+};
+
+const Trials = ({ trial }) => {
+    return (
+      <li class="passives" style="list-style-type:none;">
+        {trial.name}
+      </li>
+    );
+};
+
+const Gems = ({ gemDetails }) => {
+    return (
+        <div>
+            <li class="passives" style="list-style-type:none;" >
+            <img src={'../../assets/gems/' + gemDetails.gem_name + '.png'}/> {gemDetails.gem_name}
+            </li>
+            <li class="passives" style="list-style-type:none;">
+            Vendor: {gemDetails.vendor}
+            </li>
+            <li class="passives" style="list-style-type:none;">
+            Required Mission: {gemDetails.mission}
+            </li>
+            <br></br>
+        </div>
+    );
+  };
+
+const ActCard = ({ data }) => {
+    return (
+      <Card class={style.actCard}>
+        <h2>{data.act}</h2>
+        <h3>Passives</h3>
+        {data.quests.map(quest => (
+          <Quests quest={quest} />
+        ))}
+        <h3>Trials</h3>
+        {data.trials.map(trial => (
+          <Trials trial={trial} />
+        ))}
+        <h3>Gems</h3>
+        {data.gems.map(details => (
+          <Gems gemDetails={details} />
+        ))}
+      </Card>
+    );
+  };
+
 export default class Passives extends Component {
 	state = {
-		response: require('./passives_with_gems.json')
+        response: require('./passives_with_gems.json')
     };
-    
-    // async componentDidMount() {
-    //     const response = await fetch('http://localhost:5000/api/gems?pastebin=5N3ZSguA');
-    //     const json = await response.json();
-    //     console.log("component",json)
-    //     this.setState({response: json})
-    // }
-
-    // const [response, setResponse] = require('./passives_with_gems.json');
-    // // const [response, setResponse] = useState(0);
-
-    // useEffect(() => {
-    //     const newResponse = asyncCall(value);
-    //     setResponse(newResponse);
-    // }, [])
 
     async asyncCall() {
         let value = document.getElementById('build').value//'xaTuiHwH'
@@ -33,60 +76,18 @@ export default class Passives extends Component {
     }
 
     render({},{response}) {
-        // let json = require('./passives.json')
-        // console.log(json2)
-        // console.log("json2",typeof json2)
         console.log("response", response)
-        // let gems = getGems('http://localhost:5000/api/gems?pastebin=xaTuiHwH')
         return(
             <div class={`${style.passives} page`}>
                 <h1>Passive and Trial Locations</h1>
-                <span>xaTuiHwH</span>
-                <input id="build"></input>
-                <button type="submit" onClick={() => { this.asyncCall() }}>Build</button>
+                <div id={style.pobInput}>
+                    <TextField id="build" label="Pastebin" outlined helperText="Enter POB Pastebin" value=""/>
+                    <Button class={style.buildButton} raised ripple onClick={() => { this.asyncCall() }}>Build</Button>
+                </div>
                 <div id={style.acts}>
-                    {response.map((data) => {
-                        return(
-                        <Card class={style.actCard}>
-                        <h2>{data.act}</h2>
-                        <h3>Passives</h3>
-                        {/* <ul style="list-style-type:none;"> */}
-                        {data.quests.map((quests) => {
-                            return (
-                                <li style="list-style-type:none;">
-                                {quests.name}{quests.skillPoints}
-                                </li>
-                            )
-                        })}
-                        {/* </ul> */}
-                        <h3>Trials</h3>
-                        {data.trials.map((trials) => {
-                            return (
-                                <li class="passives" style="list-style-type:none;">
-                                {trials.name}
-                                </li>
-                            )
-                        })}
-                        <h3>Gems</h3>
-                        {data.gems.map((details) => {
-                            return (
-                                <div>
-                                <li class="passives" style="list-style-type:none;">
-                                Gem: {details.gem_name}
-                                </li>
-                                <li class="passives" style="list-style-type:none;">
-                                Vendor: {details.vendor}
-                                </li>
-                                <li class="passives" style="list-style-type:none;">
-                                Required Mission: {details.mission}
-                                </li>
-                                <br></br>
-                                </div>
-                            )
-                        })}
-                        </Card>
-                        )
-                    })}
+                    {response.map(data => (
+                    <ActCard data={data} />
+                    ))}
                 </div>
                 {/* https://pastebin.com/raw/xaTuiHwH */}
 			</div>
