553ec114ccd74ea4d82e35ce164dd68abfd3d8b1
Nick Walton <nicholas.h.walton@gmail.com>
2020-02-04 18:05:03 -0600

    working on state changes

--- site/src/routes/syndicate/index.js
+++ site/src/routes/syndicate/index.js
@@ -6,28 +6,37 @@ import 'preact-material-components/Button/style.css';
 import style from './style';
 import defaultJson from './table.json';
 
+//move format over to Blake's to have access to setState
+//try including syndicateJson in export default Passives = (syndicateJson) =>
+
 const handleClick = (e) => {
+	// const cellIndex = defaultJson.find(x => x.title === e.currentTarget.dataset.title)
+	console.log(cellIndex)
 	console.log(e.currentTarget.className)
 	e.currentTarget.className = (e.currentTarget.className == style.row ? style.green :
 								(e.currentTarget.className == style.green ? style.yellow :
 								(e.currentTarget.className == style.yellow ? style.red :
 								style.row
-	)))
+								)))
 };
 
-const RowCell = ({ cellData }) => {
-	const defaultClass = (cellData.class == "" ? style.row :
+const updateState = () => {
+
+}
+
+const RowCell = ({ cellData , rowName }) => {
+	// console.log(defaultJson)
+	const cellTitle = cellData.title
+	const defaultClass = 
+							(cellData.class == "" ? style.row :
 							(cellData.class == "green" ? style.green :
-								(cellData.class == "yellow" ? style.yellow : style.red)
+							(cellData.class == "yellow" ? style.yellow : style.red)
 							))
     return (
 		<div
 			onClick = { e => handleClick(e) }
-			data-default = {defaultClass}
-			className = {cellData.class == "" ? style.row :
-						(cellData.class == "green" ? style.green :
-							(cellData.class == "yellow" ? style.yellow : style.red)
-						)}
+			data-title = {cellTitle}
+			className = {defaultClass}
 					>
 			<img
 				style="max-width:100%;"
@@ -37,12 +46,17 @@ const RowCell = ({ cellData }) => {
     );
 };
 
-const TableRow = ({ row }) => {
+const TableRow = ({ row , rowName }) => {
+	console.log(rowName)
     return (
 		<div class={style.rowWrapper}>
-			{row.cells.map(cell => (
-			<RowCell cellData={cell} />
-			))}
+			{row.map(function(cell) {
+				return(
+				<RowCell cellData={cell} rowName={rowName}/>
+				)
+			}
+			)
+			}
 		</div>
     );
 };
@@ -64,9 +78,17 @@ export default class Syndicate extends Component {
 				<h1>Syndicate route</h1>
 				{/* <Button onClick={() => this.resetColors()}>Reset</Button> */}
 				<div>
-                    {syndicateJson.map(row => (
-                    	<TableRow row={row} />
-                    ))}
+                    {Object.keys(syndicateJson).map(function(key) {
+						return (
+							<TableRow row={syndicateJson[key]} rowName={key}/>
+						)
+					}
+					
+					)
+					// 	row => (
+                    // 	<TableRow row={row} />
+					// ))
+					}
                 </div>
 			</div>
 		);
